1674c24ba1d14bfbcd4f9caaaed458a727699b10
ezegonmac <ezegonmac01@gmail.com>
2023-02-23 12:32:51 +0100

    Fix scripts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,8 @@
 import mainStyles from '@/styles/main.css'
 import responsiveStyles from '@/styles/responsive.css'
 import animationStyles from '@/styles/animation.css'
-// import fontawesome from '@/vendors/font-awesome/css/all.min.css'
-// import swiper from '@/vendors/swiper/swiper.css'
-// import magnificPopup from '@/vendors/magnific-popup/magnific-popup.css'
+import fontAwesome from '@/vendors/font-awesome/css/all.min.css'
+import swiper from '@/vendors/swiper/swiper.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
